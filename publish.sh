3d27c51cd5cccaa876b622d2103f59d82d3b13fa
Lorenzo Delana <lorenzo.delana@gmail.com>
2024-02-17 13:07:56 +0100

    publish

--- publish.sh
+++ publish.sh
@@ -2,7 +2,7 @@
 
 exdir=$(dirname `readlink -f "$0"`)
 
-cd "$exdir"/netcore-docx
+cd "$exdir"/src/docx
 rm -fr bin
 dotnet pack -c Release
 dotnet nuget push bin/Release/*.nupkg -k $(cat ~/security/nuget-api.key) -s https://api.nuget.org/v3/index.json
